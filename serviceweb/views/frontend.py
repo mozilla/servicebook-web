a31b2f754e6ea4a046e4d609ae89fe40ee955a25
Tarek Ziadé <tarek@ziade.org>
2017-01-06 12:54:51 +0000

    new field

--- serviceweb/views/frontend.py
+++ serviceweb/views/frontend.py
@@ -7,5 +7,5 @@ frontend = Blueprint('frontend', __name__)
 
 @frontend.route("/")
 def home():
-    projects = frontend.app.db.get_entries('project', order_by='name')
+    projects = frontend.app.db.get_entries('project', sort='name')
     return render_template('home.html', projects=projects)
