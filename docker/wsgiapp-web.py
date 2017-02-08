1e26e648611b02ac28be60f71f89aa5e908ea6b1
Tarek Ziade <tarek@ziade.org>
2017-02-08 16:49:12 +0100

    fixed the module name

--- docker/wsgiapp-web.py
+++ docker/wsgiapp-web.py
@@ -2,16 +2,11 @@ import site
 import sys
 import os
 import codecs
-from werkzeug.wsgi import DispatcherMiddleware
 from serviceweb.server import create_app
-from servicebook.server import create_app as create_api
 
 
 ini = os.path.join(os.path.dirname(__file__), 'serviceweb.ini')
 application = create_app(ini_file=ini)
-wsgi = application.wsgi_app
-api = create_api(ini_file=ini)
-application.wsgi_app = DispatcherMiddleware(wsgi, {'/api': api.wsgi_app})
 
 
 if __name__ == "__main__":
